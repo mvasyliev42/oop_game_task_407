0748045b1a982d5fe927bf7025ca88fff6672f9e
Yaroslave Dubrovskiy <yaroslav112004@gmail.com>
2023-12-27 09:19:06 +0200

    create socketserver.py

---
+++ services/socket/socketserver.py
@@ -0,0 +1,22 @@
+from services.socket.basesocketserver import BaseSocketServer
+
+
+class SocketServer(BaseSocketServer):
+
+    def connectPlayer1(self):
+        self._connect1, address = self._socket.accept()
+
+    def connectPlayer2(self):
+        self._connect2, address = self._socket.accept()
+
+    def sendMessagesPlayer1(self, messages):
+        self._connect1.send(messages.encode())
+
+    def sendMessagesPlayer2(self, messages):
+        self._connect2.send(messages.encode())
+
+    def recvMessagesPlayer1(self):
+        return self._connect1.recv(1024).decode()
+
+    def recvMessagesPlayer2(self):
+        return self._connect2.recv(1024).decode()
