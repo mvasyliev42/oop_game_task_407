2001bb308ebeb306cd23a10fec24c2fe90849263
nightpacifist <nightpacifist@gmail.com>
2024-01-17 09:18:19 +0200

    telegram example

--- maint.py
+++ maint.py
@@ -1,6 +1,7 @@
 import logging
-from telegram import Update
+from telegram import Update, Bot
 from telegram.ext import filters, ApplicationBuilder, MessageHandler, ContextTypes, CommandHandler
+import asyncio
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -13,9 +14,19 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+async def send_message(bot_token, chat_id, text):
+    bot = Bot(bot_token)
+    await bot.send_message(chat_id=chat_id, text=text)
 
 if __name__ == '__main__':
+    #asyncio.run(send_message('5732842940:AAERTmbxxw20fQniRntUkQ3m-ROPUS3t_xw', 405301731, "tergfdg"))
+
+
     application = ApplicationBuilder().token('5732842940:AAERTmbxxw20fQniRntUkQ3m-ROPUS3t_xw').build()
+
+    # bot = Bot('5732842940:AAERTmbxxw20fQniRntUkQ3m-ROPUS3t_xw')
+    #
+    # bot.send_message(chat_id=405301731, text="tergfdg")
 
     start_handler = CommandHandler('start', start)
 
