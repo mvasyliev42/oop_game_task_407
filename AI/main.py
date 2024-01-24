23fddc54f05100ae6c5578fd2196e566a5dd9fb9
WHOISMDI <minenkov201621@gmail.com>
2024-01-24 08:27:50 +0100

    get to url and set to url function created

--- AI/main.py
+++ AI/main.py
@@ -7,12 +7,16 @@ import hashlib
 
 # Task 1
 def set_url_to_file(prompt, url):
-    pass
+    with open('src/url_file.csv', 'a') as file_with_url:
+        file_with_url.write(f'{prompt}|{url}\n')
 
 
 def get_url_to_file(prompt):
-    pass
-
+    with open('src/url_file.csv') as file_to_read:
+        for item in file_to_read.readlines():
+            if item.startswith(prompt):
+                return item.split('|')[1]
+    return False
 
 # Task 2
 
@@ -34,24 +38,26 @@ def md5_hash(string):
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     client = OpenAI(api_key="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")
 
-
+    url_result = get_url_to_file(update.message.text)
+    if url_result:
+        await context.bot.send_photo(chat_id=update.effective_chat.id, photo=url_result)
+    else:
+        response = client.images.generate(
+            model="dall-e-2",
+            prompt=update.message.text,
+            size="1024x1024",
+            quality="standard",
+            n=1,
+        )
+        set_url_to_file(update.message.text, response.data[0].url)
+        await context.bot.send_photo(chat_id=update.effective_chat.id, photo=response.data[0].url)
     # todo: Чи є цей промт в файлі з посиланнями, якщо є, то віддаєм з файлу, якщо немає, то генеруємо
-
-
-    response = client.images.generate(
-        model="dall-e-2",
-        prompt=update.message.text,
-        size="1024x1024",
-        quality="standard",
-        n=1,
-    )
 
     # todo: зберегти цей файл
 
     # todo: Зберігаємо промпт з юрл адресою в файл
 
-    print(response.data[0].url)
-    await context.bot.send_photo(chat_id=update.effective_chat.id, photo=response.data[0].url)
+
 
     #await context.bot.send_message(chat_id=update.effective_chat.id, text=response.data[0].url)
     ##photo = FileInpute()
